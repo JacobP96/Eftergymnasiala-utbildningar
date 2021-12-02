761c80bc12d7838b7eccd90c84a224e108b1e20f
Jacob Persson <jacobpersson96@gmail.com>
2021-12-02 12:11:04 +0100

    Javascript

--- main.js
+++ main.js
@@ -38,6 +38,73 @@ button.addEventListener("click", () => {
       studentList.appendChild(studentName);
     });
   });
+  // filtrerar listan efter ålder, yngst först i listan
+  let ageButton = document.querySelector("#showAge");
+
+  ageButton.addEventListener("click", () => {
+    studentList.innerHTML = "";
+    let sortByAge = students.sort((age1, age2) => age1.age - age2.age);
+    sortByAge.forEach((studentAge) => {
+      let studentAgeName = document.createElement("li");
+      studentAgeName.textContent = `${studentAge.firstName} ${studentAge.lastName}, ${studentAge.age} years of age`;
+      let ProgramAgeHeader = document.querySelector("#Header");
+      ProgramAgeHeader.textContent = ` Eleverna är sorterade efter ålder `;
+      let studentAgeList = document.querySelector("#userList");
+      studentAgeList.appendChild(studentAgeName);
+    });
+  });
+  // sortera efter förnamn, bokstavsordning
+  let firstNameButton = document.querySelector("#showfirstName");
+
+  firstNameButton.addEventListener("click", () => {
+    studentList.innerHTML = "";
+
+    // function för att sortera förnamn
+    function compareFirstNames(a, b) {
+      if (a.firstName < b.firstName) {
+        return -1;
+      }
+      if (a.firstName > b.firstName) {
+        return 1;
+      }
+      return 0;
+    }
+    // skriver ut eleverna i bokstavsordning baserat på förnamnet
+    let checkFirstName = students.sort(compareFirstNames);
+    checkFirstName.forEach((studentName) => {
+      let studentfirstName = document.createElement("li");
+      studentfirstName.textContent = `${studentName.firstName} ${studentName.lastName}`;
+      let ProgramNameHeader = document.querySelector("#Header");
+      ProgramNameHeader.textContent = ` Eleverna har sorterats i bokstavsordning baserat på förnamn `;
+      let studentNameList = document.querySelector("#userList");
+      studentNameList.appendChild(studentfirstName);
+    });
+  });
+  let lastNameButton = document.querySelector("#showlastName");
+  lastNameButton.addEventListener("click", () => {
+    studentList.innerHTML = "";
+
+    // function för att sortera förnamn
+    function compareLastNames(a, b) {
+      if (a.lastName < b.lastName) {
+        return -1;
+      }
+      if (a.lastName > b.lastName) {
+        return 1;
+      }
+      return 0;
+    }
+    // skriver ut eleverna i bokstavsordning baserat på förnamnet
+    let checkLastName = students.sort(compareLastNames);
+    checkLastName.forEach((studentName) => {
+      let studentlastName = document.createElement("li");
+      studentlastName.textContent = `${studentName.firstName} ${studentName.lastName}`;
+      let ProgramlastNameHeader = document.querySelector("#Header");
+      ProgramlastNameHeader.textContent = ` Eleverna har sorterats i bokstavsordning baserat på efternamn `;
+      let studentLastNameList = document.querySelector("#userList");
+      studentLastNameList.appendChild(studentlastName);
+    });
+  });
 }
 
       findData()
